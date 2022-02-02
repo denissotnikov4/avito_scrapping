992351f00009f923972b849fea613097ac5ca48c
denissotnikov4 <68634552+denissotnikov4@users.noreply.github.com>
2022-02-02 16:54:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def handle_price(array):
     return array
 
 def get_price_from_one_page(url):
-    price = []
     page = requests.get(url)
     filteredPrice = []
 
@@ -26,9 +25,29 @@ def get_price_from_one_page(url):
 
     return map(int, filteredPrice)
 
+# 1 .. count_of_pages
+def get_price_from_multiple_pages(url_general, count_of_pages):
+    price = []
+    for page in range(1, count_of_pages + 1):
+        price_text = []
+        req = requests.get(url + '&p=' + str(page))
+        soup = BeautifulSoup(req.text, 'html.parser')
+        all_items = soup.findAll('div', class_='iva-item-priceStep-uq2CQ')
+
+        for data in all_items:
+            if data.find('span', class_='price-text-_YGDY text-text-LurtD text-size-s-BxGpL') is not None:
+                price_text.append(data.text)
+
+        price_text = handle_price(price_text)
+
+        price = price + price_text
+    return map(int, price)
+
 def plotting(array):
     fig, ax = plt.subplots()
     ax.hist(array, bins=20, linewidth=0.5, edgecolor="white")
+    ax.set_xlabel('Цена')
+    ax.set_ylabel('Количество')
     plt.show()
 
 def get_statistic_information(array):
@@ -36,16 +55,9 @@ def get_statistic_information(array):
     return array.describe()
 
 
-
-
 url = "https://www.avito.ru/ekaterinburg/avtomobili/vaz_lada/2107-ASgBAgICAkTgtg3GmSjitg3Omig?cd=1&radius=200"
 
-array = sorted(get_price_from_one_page(url))
-
+array = sorted(get_price_from_multiple_pages(url, 4))
 print(get_statistic_information(array))
+
 print(plotting(array))
-
-
-
-
-
